acd25777326159d961c82319d5251f77d657b4d2
mohan1016 <chundurinani10@gmail.com>
2025-08-26 16:27:10 +0530

    Update package.json and App.js

--- src/App.js
+++ src/App.js
@@ -25,8 +25,42 @@
 // export default App;
 
 
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/navbar";
+// import Footer from "./components/Footer";
+// import HomePage from "./pages/HomePage";
+// import AddRecipePage from "./pages/AddRecipePage";
+// import EditRecipePage from "./pages/EditRecipePage";
+// import RecipeDetailPage from "./pages/RecipeDetailPage";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="d-flex flex-column min-vh-100">
+//         <Navbar />
+
+      
+//         <div className="flex-grow-1">
+//           <Routes>
+//             <Route path="/" element={<HomePage />} />
+//             <Route path="/add" element={<AddRecipePage />} />
+//             <Route path="/edit/:id" element={<EditRecipePage />} />
+//             <Route path="/recipe/:id" element={<RecipeDetailPage />} />
+//           </Routes>
+//         </div>
+
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // No BrowserRouter here
 import Navbar from "./components/navbar";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
@@ -36,23 +70,20 @@ import RecipeDetailPage from "./pages/RecipeDetailPage";
 
 function App() {
   return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <Navbar />
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
 
-      
-        <div className="flex-grow-1">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/add" element={<AddRecipePage />} />
-            <Route path="/edit/:id" element={<EditRecipePage />} />
-            <Route path="/recipe/:id" element={<RecipeDetailPage />} />
-          </Routes>
-        </div>
-
-        <Footer />
+      <div className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/add" element={<AddRecipePage />} />
+          <Route path="/edit/:id" element={<EditRecipePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetailPage />} />
+        </Routes>
       </div>
-    </Router>
+
+      <Footer />
+    </div>
   );
 }
 
